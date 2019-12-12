9b5723bae724511ac450dcbcb10e117ff81832e3
Matt Ao <mattyayoh@gmail.com>
2019-12-12 12:31:34 -0800

    Initial Commit

---
+++ FireDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FireDemo
+//
+//  Created by Matt Ao on 12/12/19.
+//  Copyright © 2019 Betty Labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
