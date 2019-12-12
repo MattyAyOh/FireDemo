91bf907f34cb6c92c55cfe21d59d693cb4d14fe8
Matt Ao <mattyayoh@gmail.com>
2019-12-12 12:38:45 -0800

    first commit

---
+++ FireDemo/NSAttributedString+.swift
@@ -0,0 +1,9 @@
+//
+//  NSAttributedString+.swift
+//  FireDemo
+//
+//  Created by Matt Ao on 12/12/19.
+//  Copyright © 2019 Betty Labs. All rights reserved.
+//
+
+import Foundation
---
+++ FireDemo/TeamBadgeView.swift
@@ -0,0 +1,9 @@
+//
+//  TeamBadgeView.swift
+//  FireDemo
+//
+//  Created by Matt Ao on 12/12/19.
+//  Copyright © 2019 Betty Labs. All rights reserved.
+//
+
+import Foundation
